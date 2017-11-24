136edcc777c27cc57c0f9be6d94a47e56fece155
sorgelig <pour.garbage@gmail.com>
2017-11-25 02:41:24 +0800

    Load ROM from SD

---
+++ releases/build_rom_LizardWizard.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set    zip=lizwiz.zip
+set ifiles=6e.cpu+6f.cpu+6h.cpu+6j.cpu+wiza+wizb+wiza+wizb+5e.cpu+5f.cpu
+set  ofile=a.lizwiz.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
